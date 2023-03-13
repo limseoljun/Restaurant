9ef56f0b0562c708c0e00f3601c7c493bc37add7
seoljunlim <seoljunlim@gmail.com>
2023-03-13 17:34:25 +0900

    검증 operservice

---
+++ src/main/java/com/example/restaurant/domain/restaurantoperator/service/validateoper/ValidateOperService.java
@@ -0,0 +1,7 @@
+package com.example.restaurant.domain.restaurantoperator.service.validateoper;
+
+import com.example.restaurant.controller.dto.oper.OperDto;
+
+public interface ValidateOperService {
+    void validte(OperDto dto);
+}
