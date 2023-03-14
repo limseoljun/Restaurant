f76fa8b1caaa3c38cc86ad40ca7867375ee7d854
seoljunlim <seoljunlim@gmail.com>
2023-03-14 23:33:37 +0900

    feat OrderMenuDTO 생성

--- src/main/java/com/example/restaurant/controller/dto/owner/OwnerDto.java
+++ src/main/java/com/example/restaurant/controller/dto/owner/OwnerDto.java
@@ -1,10 +1,10 @@
-package com.example.restaurant.controller.dto.oper;
+package com.example.restaurant.controller.dto.owner;
 
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 @Data
-public class OperDto {
+public class OwnerDto {
     @NotBlank(message = "need id")
     private final String name;
     @NotBlank(message = "need password")
@@ -12,13 +12,13 @@ public class OperDto {
     private final String restaurantName;
     private final String restaurantAddress;
 
-    public OperDto(String name,String password,String restaurantName,String restaurantAddress){
+    public OwnerDto(String name, String password, String restaurantName, String restaurantAddress){
         this.name=name;
         this.password=password;
         this.restaurantName=restaurantName;
         this.restaurantAddress=restaurantAddress;
     }
-    public OperDto(String name,String password){
+    public OwnerDto(String name, String password){
         this.name=name;
         this.password=password;
         restaurantName = null;
