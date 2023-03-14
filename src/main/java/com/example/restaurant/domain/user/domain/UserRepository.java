21898f4652b85b8ff479ab3a05c98f611ef601e3
seoljunlim <seoljunlim@gmail.com>
2023-03-14 23:39:00 +0900

    feat UserRepository 변경

--- src/main/java/com/example/restaurant/domain/user/domain/UserRepository.java
+++ src/main/java/com/example/restaurant/domain/user/domain/UserRepository.java
@@ -1,10 +1,31 @@
 package com.example.restaurant.domain.user.domain;
 
+import com.example.restaurant.exception.UserIdNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
     boolean existsByName(String name);
 
     boolean existsByPassword(String password);
+
+    Optional<User> findByName(String name);
+
+    default User validateName(String name){
+            Optional<User> optionalUser = findByName(name);
+            return optionalUser.orElseThrow(() ->
+                    new UsernameNotFoundException("Invalid User Name")
+            );
+
+    }
+
+    default User validateUser(Long id){
+        Optional<User> optionalUser = findById(id);
+        return optionalUser.orElseThrow(() ->
+                new UserIdNotFoundException()
+        );
+    };
 }
