2fb9e9bf696fecc9620d8bf43ec8177e7e435b54
seoljunlim <seoljunlim@gmail.com>
2023-03-14 23:38:22 +0900

    Test 유저 로그인 회원가입 테스트

--- src/test/java/com/example/restaurant/domain/user/service/join/UserJoinServiceTest.java
+++ src/test/java/com/example/restaurant/domain/user/service/join/UserJoinServiceTest.java
@@ -1,13 +1,44 @@
 package com.example.restaurant.domain.user.service.join;
 
+import com.example.restaurant.controller.dto.user.UserDto;
+import com.example.restaurant.domain.user.domain.User;
+import com.example.restaurant.domain.user.domain.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest
 @Transactional
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserJoinServiceTest {
+    private final UserRepository userRepository;
+    private final UserJoinService userJoinService;
+    private final PasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    UserJoinServiceTest(UserRepository userRepository, UserJoinService userJoinService, PasswordEncoder bCyptPasswordEncoder, PasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.userJoinService = userJoinService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
 
+    @Test
+    void 유저_회원가입_정상작동(){
+        UserDto dto = new UserDto("user","1234","nick","phone","home");
+
+        userJoinService.join(dto);
+        User user = userRepository.findByName(dto.getName()).get();
+
+        assertEquals(user.getName(),dto.getName());
+        assertNotEquals(user.getPassword(),dto.getPassword());
+        assertEquals(user.getNickName(),dto.getNickName());
+        assertEquals(user.getPhoneNum(),dto.getPhoneNum());
+        assertEquals(user.getHomeAddress(),dto.getHomeAddress());
+    }
 }
--- src/test/java/com/example/restaurant/domain/user/service/login/UserLoginServiceTest.java
+++ src/test/java/com/example/restaurant/domain/user/service/login/UserLoginServiceTest.java
@@ -1,13 +1,78 @@
 package com.example.restaurant.domain.user.service.login;
 
+import com.example.restaurant.controller.dto.user.UserDto;
+import com.example.restaurant.domain.user.domain.User;
+import com.example.restaurant.domain.user.domain.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserLoginServiceTest {
+    private final UserRepository userRepository;
+    private final UserLoginService userLoginService;
+    private final PasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    UserLoginServiceTest(UserRepository userRepository, UserLoginService userLoginService, PasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.userLoginService = userLoginService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
 
+    @Test
+    void 유저_로그인_정상작동(){
+        User user = new User("user",bCryptPasswordEncoder.encode("1234"),"nick","phone","123");
+        userRepository.save(user);
+        UserDto dto = new UserDto("user","1234");
+        Long loginId = userLoginService.validate(dto);
+        assertEquals(user.getId(),loginId);
+    }
+
+    @Test
+    void 유저_아이디_다름(){
+        User user = new User("user",bCryptPasswordEncoder.encode("1234"),"nick","phone","123");
+        userRepository.save(user);
+
+        UserDto dto = new UserDto("user1","1234");
+        Exception e = assertThrows(UsernameNotFoundException.class, () ->
+                userLoginService.validate(dto)
+        );
+
+        assertEquals(e.getMessage(),"Invalid User Name");
+    }
+
+    @Test
+    void 유저_비밀번호_다름(){
+        User user = new User("user",bCryptPasswordEncoder.encode("1234"),"nick","phone","123");
+        userRepository.save(user);
+
+        UserDto dto = new UserDto("user","12345");
+        Exception e = assertThrows(BadCredentialsException.class, () ->
+                userLoginService.validate(dto)
+        );
+
+        assertEquals(e.getMessage(),"Invalid User Password");
+    }
+
+    @Test
+    void 유저_아이디_비밀번호_다름(){
+        User user = new User("user",bCryptPasswordEncoder.encode("1234"),"nick","phone","123");
+        userRepository.save(user);
+
+        UserDto dto = new UserDto("user1",bCryptPasswordEncoder.encode("12345"));
+        Exception e = assertThrows(UsernameNotFoundException.class, () ->
+                userLoginService.validate(dto)
+        );
+        assertEquals(e.getMessage(),"Invalid User Name");
+    }
 }
