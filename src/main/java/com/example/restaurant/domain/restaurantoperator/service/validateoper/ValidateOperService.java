62b75bbb431cc798806d2577c6841b1288131f0c
seoljunlim <seoljunlim@gmail.com>
2023-03-14 23:34:45 +0900

    삭제

--- src/main/java/com/example/restaurant/domain/restaurantoperator/service/validateoper/ValidateOperService.java
+++
@@ -1,7 +0,0 @@
-package com.example.restaurant.domain.restaurantoperator.service.validateoper;
-
-import com.example.restaurant.controller.dto.oper.OperDto;
-
-public interface ValidateOperService {
-    void validte(OperDto dto);
-}
