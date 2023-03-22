d97f05f2efd6aef32aa26bc02d50c8d05761db2a
seoljunlim <seoljunlim@gmail.com>
2023-03-22 09:30:52 +0900

    삭제

--- src/main/java/com/example/restaurant/domain/order/info/domain/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.restaurant.domain.order.info.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Long> {
-
-    Order validateOrder(Long orderId);
-}
