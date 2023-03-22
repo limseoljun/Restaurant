32ffbcb8f13db549931fc9eaa2d894fbbb246f11
seoljunlim <seoljunlim@gmail.com>
2023-03-22 09:09:24 +0900

    feat FoodRepository 수정

--- src/main/java/com/example/restaurant/domain/food/domain/FoodRepository.java
+++ src/main/java/com/example/restaurant/domain/food/domain/FoodRepository.java
@@ -1,5 +1,6 @@
 package com.example.restaurant.domain.food.domain;
 
+import com.example.restaurant.exception.NotFoundFoodException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
@@ -7,9 +8,14 @@ import org.springframework.data.repository.query.Param;
 
 import javax.persistence.LockModeType;
 import java.util.List;
+import java.util.Optional;
 
 public interface FoodRepository extends JpaRepository<Food,Long> {
-    List<Food> findByOwnerId(Long ownerId);
+    List<Food> findByRestaurantId(Long restaurantId);
 
-    Food validateFood(Long foodId);
+    default Food validateFood(Long foodId){
+        Optional<Food> foodMenuOptional = findById(foodId);
+
+        return foodMenuOptional.orElseThrow(NotFoundFoodException::new);
+    };
 }
